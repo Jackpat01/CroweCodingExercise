29021b00b723fa00a7bc64ba4d838982112f89d5
DESKTOP-RUDVCMB\jackp <jackpat01@gmail.com>
2017-01-31 14:54:22 -0500

    Add project files.

---
+++ CroweCodingExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweCodingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
